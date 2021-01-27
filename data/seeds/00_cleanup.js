f98e670de81deb2764c43085780221175846de7e
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2021-01-26 23:42:26 -0500

    db file is ready

--- data/seeds/00_cleanup.js
+++ data/seeds/00_cleanup.js
@@ -1,6 +1,7 @@
 
 exports.seed = async function(knex) {
   await knex("users").truncate();
+  await knex("recipe_ingredients").truncate();
   await knex("ingredients").truncate();
   await knex("recipes").truncate();
 };
---
+++ data/seeds/03_recipe_ingredients.js
@@ -0,0 +1,15 @@
+
+exports.seed = async function(knex) {
+  await knex("recipe_ingredients").insert([
+    {recipe_id: 1, ingredients_id: 1, quantity: "2-Cups"},
+    {recipe_id: 1, ingredients_id: 2, quantity: "1-1/2-Cups"},
+    {recipe_id: 1, ingredients_id: 3, quantity: "2-tsp"},
+    {recipe_id: 1, ingredients_id: 4, quantity: "1-tsp"},
+    {recipe_id: 1, ingredients_id: 5, quantity: "1/2-tsp"},
+    {recipe_id: 1, ingredients_id: 6, quantity: "1/2-tsp"},
+    {recipe_id: 1, ingredients_id: 7, quantity: "1-No"},
+    {recipe_id: 1, ingredients_id: 8, quantity: "1-1/4-Cups"},
+    {recipe_id: 1, ingredients_id: 9, quantity: "2-tbsp"},
+    {recipe_id: 1, ingredients_id: 10, quantity: "2-tsp"},
+  ])
+};
